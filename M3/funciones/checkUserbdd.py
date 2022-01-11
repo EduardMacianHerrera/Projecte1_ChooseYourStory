f4409675df4f9c068caf45baf676ea21d574ff9f
EduardMacianHerrera <EMacianHerrera.cf@iesesteveterradas.cat>
2022-01-11 12:08:56 +0100

    Funcion checkUserbdd.py

--- M3/funciones/checkUserbdd.py
+++ M3/funciones/checkUserbdd.py
@@ -1,14 +1,30 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="debiansql.westeurope.cloudapp.azure.com",
-  user="edu",
-  password="edu"
-)
+user = input("User: ")
+password = input("Password: ")
 
-print(mydb)
+def checkUserbdd(user,password):
+  mydb = mysql.connector.connect(
+    host="debiansql.westeurope.cloudapp.azure.com",
+    user="edu",
+    password="edu",
+    database="project"
+  )
 
+  mycursor = mydb.cursor()
+  mycursor.execute("select username from USER")
+  myresult = mycursor.fetchall()
 
-mycursor = mydb.cursor()
+  user = (user,)
+  if user not in myresult:
+    return 0
 
-print(mycursor.execute("select * from project.USER;"))
+  mycursor.execute(f"select password from USER where username = '{user[0]}'")
+  myresult = mycursor.fetchall()
+
+  password = (password,)
+
+  if password not in myresult:
+    return -1
+  else:
+    return 1
