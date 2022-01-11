fecab994ce6bcceae927b2997f3d517fc24e2663
Sergio <sergioherreramarin@gmail.com>
2022-01-11 13:27:01 +0100

    Commit casa

--- M3/funciones/getFormatedAdventures.py
+++ M3/funciones/getFormatedAdventures.py
@@ -1,10 +1,8 @@
-
-
 #Get formated adventures
 
 dic_adventures ={
 1:{"Name":"Aventura 1", "Description":"Es la aventura 1", "Charachers":[1, 4, 5]},
-2:{"Name":"Aventura 2", "Description":"Es la aventura 2", "Charachers":[3, 6, 7]},
+2:{"Name":"Aventura 2", "Description":"Es la aventura 2 iyiuy uyiuyui yuiy uyiuouhihii", "Charachers":[3, 6, 7]},
 3:{"Name":"Aventura 3", "Description":"Es la aventura 3", "Charachers":[2, 5, 9]}
 }
 
@@ -19,10 +17,7 @@ def getFormatedAdventures(adventures):
     for i in adventures:
         dictData += "\n" + str(i).ljust(20) + adventures[i]["Name"].ljust(20) + \
                    adventures[i]["Description"].ljust(20)
-
     return header + dictData
 
-
 print(getFormatedAdventures(dic_adventures))
-
 
