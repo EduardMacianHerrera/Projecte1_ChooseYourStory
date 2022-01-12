a13bc6ed65cb825f0e28ce911783b7630aa27644
ihernandezvelarde <neeiihernandez@gmail.com>
2022-01-12 12:58:47 +0100

    Insertar personajes, Historias
    Inserción Personajes y Historias

---
+++ M2/InsertCharacters.sql
@@ -0,0 +1,15 @@
+use project;
+
+INSERT INTO CHARACTERS(character_name, description, user_creation, date_creation, date_mod, user_mod)
+ VALUES('Vegeta','Príncipe de una raza guerrera extraterrestre conocida como los Saiyajin.', user(), now(), now(), user());
+ 
+INSERT INTO CHARACTERS(character_name, description, user_creation, date_creation, date_mod, user_mod)
+ VALUES('Itachi','Ninja renegado considerado un prodigio y portador de numerosas cualidades sobresalientes', user(), now(), now(), user());
+ 
+INSERT INTO CHARACTERS(character_name, description, user_creation, date_creation, date_mod, user_mod)
+ VALUES('Zoro','Espadachín experto en el Santōryū (estilo de tres espadas) Valiente, fuerte y bastante temperamental', user(), now(), now(), user());
+ 
+INSERT INTO CHARACTERS(character_name, description, user_creation, date_creation, date_mod, user_mod)
+ VALUES('Jack Sparrow','Legendario pirata de Todos los Mares y un irreverente embaucador del Caribe.', user(), now(), now(), user());
+ 
+ 
