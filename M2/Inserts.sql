8ff866fb2d477152efbdc680c0ad1dfaa2f93210
EduardMacianHerrera <EMacianHerrera.cf@iesesteveterradas.cat>
2022-01-13 12:30:32 +0100

    Scripts prototipo y bbdd revisada

--- M2/Inserts.sql
+++ M2/Inserts.sql
@@ -1,25 +1,64 @@
 use project;
 
-# Inserts ADVENTURE
+delete from `OPTION`;
+delete from STEP;
 delete from ADVENTURE;
+delete from `CHARACTER`;
+delete from STARS;
 
-insert into ADVENTURE(adventure_name,description,user_creation,date_creation,date_mod,user_mod) values 
+
+# Inserts CHARACTERS
+
+INSERT INTO `CHARACTER`(character_name, description, user_creation, date_creation) VALUES 
+('Vegeta','Príncipe de una raza guerrera extraterrestre conocida como los Saiyajin.', user(), now()),
+('Itachi','Ninja renegado considerado un prodigio y portador de numerosas cualidades sobresalientes', user(), now()),
+('Zoro','Espadachín experto en el Santōryū (estilo de tres espadas) Valiente, fuerte y bastante temperamental', user(), now()),
+('Jack Sparrow','Legendario pirata de Todos los Mares y un irreverente embaucador del Caribe.', user(), now());
+
+# Inserts ADVENTURE
+
+insert into ADVENTURE(adventure_name,description,user_creation,date_creation) values 
 # Historia 1
-("En busca de la gráfica perdida","Busca el tesoro oculto, disfruta de una aventura a través de las mazmorras Morgul. Encuentra, enfrenta y supera cada desafío que te encuentres por el camino hasta descubrir que final es el que te espera.",user(),now(),now(),user()),
+("En busca de la gráfica perdida","Busca el tesoro oculto, disfruta de una aventura a través de las mazmorras Morgul. Encuentra, enfrenta y supera cada desafío que te encuentres por el camino hasta descubrir que final es el que te espera.",user(),now()),
 #Historia 2
-("Un examen desastroso","Te despiertas un día de examen, no te ha sonado la alarma. Decide que vas a hacer, pero cuidado con las decisiones que tomas, toda acción tiene su consecuencia ya sea para bien o para mal.",user(),now(),now(),user()),
+("Un examen desastroso","Te despiertas un día de examen, no te ha sonado la alarma. Decide que vas a hacer, pero cuidado con las decisiones que tomas, toda acción tiene su consecuencia ya sea para bien o para mal.",user(),now()),
 #Historia 3
-("Póker interdimensional","Juega una o varias partidas de poker interdimensional. Recuerda que no todo depende de la mano que te toque sino de como juegues tus cartas. A veces retirarse a tiempo es una victoria (o no).",user(),now(),now(),user());
+("Póker interdimensional","Juega una o varias partidas de poker interdimensional. Recuerda que no todo depende de la mano que te toque sino de como juegues tus cartas. A veces retirarse a tiempo es una victoria (o no).",user(),now());
 
 # Inserts STEP
-delete from STEP;
+insert into STARS(ID_ADVENTURE,ID_CHARACTER) values 
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "En busca de la gráfica perdida"),(select ID_CHARACTER from `CHARACTER` where character_name = "Jack Sparrow")),
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "Un examen desastroso"),(select ID_CHARACTER from `CHARACTER` where character_name = "Zoro")),
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "Un examen desastroso"),(select ID_CHARACTER from `CHARACTER` where character_name = "Itachi")),
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "Un examen desastroso"),(select ID_CHARACTER from `CHARACTER` where character_name = "Jack Sparrow")),
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "Póker interdimensional"),(select ID_CHARACTER from `CHARACTER` where character_name = "Vegeta")),
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "Póker interdimensional"),(select ID_CHARACTER from `CHARACTER` where character_name = "Zoro")),
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "Póker interdimensional"),(select ID_CHARACTER from `CHARACTER` where character_name = "Itachi")),
+((select ID_ADVENTURE from ADVENTURE where adventure_name = "Póker interdimensional"),(select ID_CHARACTER from `CHARACTER` where character_name = "Jack Sparrow"))
+;
 
-insert into STEP(description,the_end,start,user_creation,date_creation,date_mod,user_mod,FK_ID_ADVENTURE) values 
+# Inserts STEP
+
+insert into STEP(description,the_end,start,user_creation,date_creation,ID_ADVENTURE) values 
 # Historia 1
 # Primer Paso
-("Estás en una misión dentro de unas mazmorras con el fin de conseguir el gran tesoro; Una RTX3090. Te encuentras un arsenal lleno de armas de todo tipo, que escoges?", 0,1,user(),now(),now(),user(),(select ID_ADVENTURE from ADVENTURE where adventure_name = "En busca de la gráfica perdida")),
- 
-("Aparece Joseph Joestar, que haces?", 0,0,user(),now(),now(),user(),(select ID_ADVENTURE from ADVENTURE where adventure_name = "En busca de la gráfica perdida")),
+("Estás en una misión dentro de unas mazmorras con el fin de conseguir el gran tesoro; Una RTX3090 Ti. Te encuentras un arsenal lleno de armas de todo tipo, que escoges?", 0,1,user(),now(),(select ID_ADVENTURE from ADVENTURE where adventure_name = "En busca de la gráfica perdida")),
+# Pasos intermedios
+("Que haces?", 0,0,user(),now(),(select ID_ADVENTURE from ADVENTURE where adventure_name = "En busca de la gráfica perdida")),
 #Final 1
-("Le caes bien, os haceis amigos y buscais la grafica juntos. Finalmente la acabas vendiendo por esteroides y te vuelves culturista. FIN", 1,0,user(),now(),now(),user(),(select ID_ADVENTURE from ADVENTURE where adventure_name = "En busca de la gráfica perdida"));
+("Le caes bien, os haceis amigos y buscais la grafica juntos. Finalmente la acabas vendiendo por esteroides y te vuelves culturista. FIN", 1,0,user(),now(),(select ID_ADVENTURE from ADVENTURE where adventure_name = "En busca de la gráfica perdida"));
 
+# Insert OPTION
+
+insert into `OPTION`(description,answer,user_creation,date_creation,ID_STEP_FROM,ID_STEP_TO) values
+("Armas de fuego","Aparece Joseph Joestar",user(),now(),(
+	select ID_STEP from STEP where description = 
+    "Estás en una misión dentro de unas mazmorras con el fin de conseguir el gran tesoro; Una RTX3090 Ti. Te encuentras un arsenal lleno de armas de todo tipo, que escoges?"),(
+    select ID_STEP from STEP where description = 
+    "Que haces?")),
+("Te pones a hacer Jojo poses con él.","",user(),now(),(
+	select ID_STEP from STEP where description = 
+    "Que haces?"),(
+    select ID_STEP from STEP where description = 
+    "Le caes bien, os haceis amigos y buscais la grafica juntos. Finalmente la acabas vendiendo por esteroides y te vuelves culturista. FIN"));
+ 
--- M2/ScriptWorkbench.sql
+++ M2/ScriptWorkbench.sql
@@ -23,39 +23,26 @@ CREATE TABLE IF NOT EXISTS `project`.`USER` (
   `password` VARCHAR(12) NOT NULL,
   `user_creation` VARCHAR(45) NOT NULL,
   `date_creation` DATETIME NOT NULL,
-  `date_mod` DATETIME NOT NULL,
-  `user_mod` VARCHAR(45) NOT NULL,
+  `date_mod` DATETIME NULL,
+  `user_mod` VARCHAR(45) NULL,
   PRIMARY KEY (`ID_USER`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `project`.`CHARACTERS`
+-- Table `project`.`CHARACTER`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project`.`CHARACTERS` (
+CREATE TABLE IF NOT EXISTS `project`.`CHARACTER` (
   `ID_CHARACTER` INT NOT NULL AUTO_INCREMENT,
   `character_name` VARCHAR(45) NOT NULL,
   `description` VARCHAR(250) NOT NULL,
   `user_creation` VARCHAR(45) NOT NULL,
   `date_creation` DATETIME NOT NULL,
-  `date_mod` DATETIME NOT NULL,
-  `user_mod` VARCHAR(45) NOT NULL,
+  `date_mod` DATETIME NULL,
+  `user_mod` VARCHAR(45) NULL,
   PRIMARY KEY (`ID_CHARACTER`),
   UNIQUE INDEX `character_name_UNIQUE` (`character_name` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `project`.`RECORD`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project`.`RECORD` (
-  `ID_RECORD` INT NOT NULL AUTO_INCREMENT,
-  `user_creation` VARCHAR(45) NOT NULL,
-  `date_creation` DATETIME NOT NULL,
-  `date_mod` DATETIME NOT NULL,
-  `user_mod` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID_RECORD`))
 ENGINE = InnoDB;
 
 
@@ -68,8 +55,8 @@ CREATE TABLE IF NOT EXISTS `project`.`ADVENTURE` (
   `description` VARCHAR(250) NOT NULL,
   `user_creation` VARCHAR(45) NOT NULL,
   `date_creation` DATETIME NOT NULL,
-  `date_mod` DATETIME NOT NULL,
-  `user_mod` VARCHAR(45) NOT NULL,
+  `date_mod` DATETIME NULL,
+  `user_mod` VARCHAR(45) NULL,
   PRIMARY KEY (`ID_ADVENTURE`))
 ENGINE = InnoDB;
 
@@ -82,34 +69,27 @@ CREATE TABLE IF NOT EXISTS `project`.`GAME` (
   `date` DATETIME NOT NULL,
   `user_creation` VARCHAR(45) NOT NULL,
   `date_creation` DATETIME NOT NULL,
-  `date_mod` DATETIME NOT NULL,
-  `user_mod` VARCHAR(45) NOT NULL,
-  `FK_ID_CHARACTER` INT NOT NULL,
-  `FK_ID_USER` INT NOT NULL,
-  `FK_ID_RECORD` INT NOT NULL,
-  `FK_ID_ADVENTURE` INT NOT NULL,
+  `date_mod` DATETIME NULL,
+  `user_mod` VARCHAR(45) NULL,
+  `ID_CHARACTER` INT NOT NULL,
+  `ID_USER` INT NOT NULL,
+  `ID_ADVENTURE` INT NOT NULL,
   PRIMARY KEY (`ID_GAME`),
-  INDEX `fk_character_game_idx` (`FK_ID_CHARACTER` ASC) VISIBLE,
-  INDEX `fk_user_game_idx` (`FK_ID_USER` ASC) VISIBLE,
-  INDEX `fk_record_game_idx` (`FK_ID_RECORD` ASC) VISIBLE,
-  INDEX `fk_adventure_game_idx` (`FK_ID_ADVENTURE` ASC) VISIBLE,
+  INDEX `fk_character_game_idx` (`ID_CHARACTER` ASC) VISIBLE,
+  INDEX `fk_user_game_idx` (`ID_USER` ASC) VISIBLE,
+  INDEX `fk_adventure_game_idx` (`ID_ADVENTURE` ASC) VISIBLE,
   CONSTRAINT `fk_character_game`
-    FOREIGN KEY (`FK_ID_CHARACTER`)
-    REFERENCES `project`.`CHARACTERS` (`ID_CHARACTER`)
+    FOREIGN KEY (`ID_CHARACTER`)
+    REFERENCES `project`.`CHARACTER` (`ID_CHARACTER`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_game`
-    FOREIGN KEY (`FK_ID_USER`)
+    FOREIGN KEY (`ID_USER`)
     REFERENCES `project`.`USER` (`ID_USER`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_record_game`
-    FOREIGN KEY (`FK_ID_RECORD`)
-    REFERENCES `project`.`RECORD` (`ID_RECORD`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_adventure_game`
-    FOREIGN KEY (`FK_ID_ADVENTURE`)
+    FOREIGN KEY (`ID_ADVENTURE`)
     REFERENCES `project`.`ADVENTURE` (`ID_ADVENTURE`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -126,21 +106,14 @@ CREATE TABLE IF NOT EXISTS `project`.`STEP` (
   `start` BIT NOT NULL,
   `user_creation` VARCHAR(45) NOT NULL,
   `date_creation` DATETIME NOT NULL,
-  `date_mod` DATETIME NOT NULL,
-  `user_mod` VARCHAR(45) NOT NULL,
-  `FK_ID_ADVENTURE` INT NOT NULL,
-  `FK_ID_STEP` INT NULL,
+  `date_mod` DATETIME NULL,
+  `user_mod` VARCHAR(45) NULL,
+  `ID_ADVENTURE` INT NOT NULL,
   PRIMARY KEY (`ID_STEP`),
-  INDEX `fk_adventure_step_idx` (`FK_ID_ADVENTURE` ASC) VISIBLE,
-  INDEX `fk_STEP_STEP1_idx` (`FK_ID_STEP` ASC) VISIBLE,
+  INDEX `fk_adventure_step_idx` (`ID_ADVENTURE` ASC) VISIBLE,
   CONSTRAINT `fk_adventure_step`
-    FOREIGN KEY (`FK_ID_ADVENTURE`)
+    FOREIGN KEY (`ID_ADVENTURE`)
     REFERENCES `project`.`ADVENTURE` (`ID_ADVENTURE`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_step_step`
-    FOREIGN KEY (`FK_ID_STEP`)
-    REFERENCES `project`.`STEP` (`ID_STEP`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -152,31 +125,51 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `project`.`OPTION` (
   `ID_OPTION` INT NOT NULL AUTO_INCREMENT,
   `description` VARCHAR(500) NOT NULL,
+  `answer` VARCHAR(1000) NOT NULL,
   `user_creation` VARCHAR(45) NOT NULL,
   `date_creation` DATETIME NOT NULL,
-  `date_mod` DATETIME NOT NULL,
-  `user_mod` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID_OPTION`))
+  `date_mod` DATETIME NULL,
+  `user_mod` VARCHAR(45) NULL,
+  `ID_STEP_FROM` INT NOT NULL,
+  `ID_STEP_TO` INT NOT NULL,
+  PRIMARY KEY (`ID_OPTION`, `ID_STEP_FROM`, `ID_STEP_TO`),
+  INDEX `fk_OPTION_STEP1_idx` (`ID_STEP_FROM` ASC) VISIBLE,
+  INDEX `fk_OPTION_STEP2_idx` (`ID_STEP_TO` ASC) VISIBLE,
+  CONSTRAINT `fk_OPTION_STEP1`
+    FOREIGN KEY (`ID_STEP_FROM`)
+    REFERENCES `project`.`STEP` (`ID_STEP`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_OPTION_STEP2`
+    FOREIGN KEY (`ID_STEP_TO`)
+    REFERENCES `project`.`STEP` (`ID_STEP`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `project`.`RECORD_HAS_OPTION`
+-- Table `project`.`RECORD`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project`.`RECORD_HAS_OPTION` (
-  `FK_ID_OPTION` INT NOT NULL,
-  `FK_ID_RECORD` INT NOT NULL,
-  PRIMARY KEY (`FK_ID_OPTION`, `FK_ID_RECORD`),
-  INDEX `fk_OPTION_has_RECORD_RECORD1_idx` (`FK_ID_RECORD` ASC) VISIBLE,
-  INDEX `fk_OPTION_has_RECORD_OPTION1_idx` (`FK_ID_OPTION` ASC) VISIBLE,
-  CONSTRAINT `fk_option_has`
-    FOREIGN KEY (`FK_ID_OPTION`)
-    REFERENCES `project`.`OPTION` (`ID_OPTION`)
+CREATE TABLE IF NOT EXISTS `project`.`RECORD` (
+  `user_creation` VARCHAR(45) NOT NULL,
+  `date_creation` DATETIME NOT NULL,
+  `date_mod` DATETIME NULL,
+  `user_mod` VARCHAR(45) NULL,
+  `ID_GAME` INT NOT NULL,
+  `ID_OPTION` INT NOT NULL,
+  `ID_STEP_FROM` INT NOT NULL,
+  `ID_STEP_TO` INT NOT NULL,
+  PRIMARY KEY (`ID_GAME`, `ID_OPTION`, `ID_STEP_FROM`, `ID_STEP_TO`),
+  INDEX `fk_RECORD_OPTION1_idx` (`ID_OPTION` ASC, `ID_STEP_FROM` ASC, `ID_STEP_TO` ASC) VISIBLE,
+  CONSTRAINT `fk_RECORD_GAME1`
+    FOREIGN KEY (`ID_GAME`)
+    REFERENCES `project`.`GAME` (`ID_GAME`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_record_has`
-    FOREIGN KEY (`FK_ID_RECORD`)
-    REFERENCES `project`.`RECORD` (`ID_RECORD`)
+  CONSTRAINT `fk_RECORD_OPTION1`
+    FOREIGN KEY (`ID_OPTION` , `ID_STEP_FROM` , `ID_STEP_TO`)
+    REFERENCES `project`.`OPTION` (`ID_OPTION` , `ID_STEP_FROM` , `ID_STEP_TO`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -186,110 +179,19 @@ ENGINE = InnoDB;
 -- Table `project`.`STARS`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `project`.`STARS` (
-  `FK_ID_ADVENTURE` INT NOT NULL,
-  `FK_ID_CHARACTER` INT NOT NULL,
-  PRIMARY KEY (`FK_ID_ADVENTURE`, `FK_ID_CHARACTER`),
-  INDEX `fk_ADVENTURE_has_CHARACTER_CHARACTER1_idx` (`FK_ID_CHARACTER` ASC) VISIBLE,
-  INDEX `fk_ADVENTURE_has_CHARACTER_ADVENTURE1_idx` (`FK_ID_ADVENTURE` ASC) VISIBLE,
+  `ID_ADVENTURE` INT NOT NULL,
+  `ID_CHARACTER` INT NOT NULL,
+  PRIMARY KEY (`ID_ADVENTURE`, `ID_CHARACTER`),
+  INDEX `fk_ADVENTURE_has_CHARACTER_CHARACTER1_idx` (`ID_CHARACTER` ASC) VISIBLE,
+  INDEX `fk_ADVENTURE_has_CHARACTER_ADVENTURE1_idx` (`ID_ADVENTURE` ASC) VISIBLE,
   CONSTRAINT `fk_adventure_stars`
-    FOREIGN KEY (`FK_ID_ADVENTURE`)
+    FOREIGN KEY (`ID_ADVENTURE`)
     REFERENCES `project`.`ADVENTURE` (`ID_ADVENTURE`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_character_stars`
-    FOREIGN KEY (`FK_ID_CHARACTER`)
-    REFERENCES `project`.`CHARACTERS` (`ID_CHARACTER`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `project`.`RECORD_HAS_STEP`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project`.`RECORD_HAS_STEP` (
-  `FK_ID_STEP` INT NOT NULL,
-  `FK_ID_RECORD` INT NOT NULL,
-  PRIMARY KEY (`FK_ID_STEP`, `FK_ID_RECORD`),
-  INDEX `fk_STEP_has_RECORD_RECORD1_idx` (`FK_ID_RECORD` ASC) VISIBLE,
-  INDEX `fk_STEP_has_RECORD_STEP1_idx` (`FK_ID_STEP` ASC) VISIBLE,
-  CONSTRAINT `fk_step_record_has_step`
-    FOREIGN KEY (`FK_ID_STEP`)
-    REFERENCES `project`.`STEP` (`ID_STEP`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_record_record_has_step`
-    FOREIGN KEY (`FK_ID_RECORD`)
-    REFERENCES `project`.`RECORD` (`ID_RECORD`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `project`.`GOES`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project`.`GOES` (
-  `FK_ID_STEP` INT NOT NULL,
-  `FK_ID_OPTION` INT NOT NULL,
-  PRIMARY KEY (`FK_ID_STEP`, `FK_ID_OPTION`),
-  INDEX `fk_STEP_has_OPTION_OPTION1_idx` (`FK_ID_OPTION` ASC) VISIBLE,
-  INDEX `fk_STEP_has_OPTION_STEP1_idx` (`FK_ID_STEP` ASC) VISIBLE,
-  CONSTRAINT `fk_step_goes`
-    FOREIGN KEY (`FK_ID_STEP`)
-    REFERENCES `project`.`STEP` (`ID_STEP`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_option_goes`
-    FOREIGN KEY (`FK_ID_OPTION`)
-    REFERENCES `project`.`OPTION` (`ID_OPTION`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `project`.`COMES`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project`.`COMES` (
-  `FK_ID_STEP` INT NOT NULL,
-  `FK_ID_OPTION` INT NOT NULL,
-  PRIMARY KEY (`FK_ID_STEP`, `FK_ID_OPTION`),
-  INDEX `fk_STEP_has_OPTION_OPTION1_idx` (`FK_ID_OPTION` ASC) VISIBLE,
-  INDEX `fk_STEP_has_OPTION_STEP1_idx` (`FK_ID_STEP` ASC) VISIBLE,
-  CONSTRAINT `fk_step_comes`
-    FOREIGN KEY (`FK_ID_STEP`)
-    REFERENCES `project`.`STEP` (`ID_STEP`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_option_comes`
-    FOREIGN KEY (`FK_ID_OPTION`)
-    REFERENCES `project`.`OPTION` (`ID_OPTION`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `project`.`ANSWER`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `project`.`ANSWER` (
-  `ID_ANSWER` INT NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(1000) NOT NULL,
-  `FK_ID_OPTION` INT NOT NULL,
-  `FK_ID_ANSWER` INT NULL,
-  `FK_ID_OPTION_ANSWER` INT NULL,
-  PRIMARY KEY (`ID_ANSWER`, `FK_ID_OPTION`),
-  INDEX `fk_option_answer_idx` (`FK_ID_OPTION` ASC) VISIBLE,
-  INDEX `fk_ANSWER_ANSWER1_idx` (`FK_ID_ANSWER` ASC, `FK_ID_OPTION_ANSWER` ASC) VISIBLE,
-  CONSTRAINT `fk_option_answer`
-    FOREIGN KEY (`FK_ID_OPTION`)
-    REFERENCES `project`.`OPTION` (`ID_OPTION`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_answer_answer`
-    FOREIGN KEY (`FK_ID_ANSWER` , `FK_ID_OPTION_ANSWER`)
-    REFERENCES `project`.`ANSWER` (`ID_ANSWER` , `FK_ID_OPTION`)
+    FOREIGN KEY (`ID_CHARACTER`)
+    REFERENCES `project`.`CHARACTER` (`ID_CHARACTER`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
