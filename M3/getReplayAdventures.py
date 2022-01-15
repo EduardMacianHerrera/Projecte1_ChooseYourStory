7a5413d0a31df4a3e49f643dac0edbfeb0a7b61d
Albertfrgo <92680605+Albertfrgo@users.noreply.github.com>
2022-01-16 00:00:45 +0100

    Intento de hacer el replayadventures, la query no ha funcionado

--- M3/getReplayAdventures.py
+++ M3/getReplayAdventures.py
@@ -2,6 +2,18 @@ from createCon import createConn
 
 def getReplayAdventures():
     conexion =createConn()
-    cursor =connexion.cursor()
+    cursor =conexion.cursor()
+    # Esta query es provisional para testear que la conexión se ha hecho correctamente,
+    # no es la del ejercicio
+    cursor.execute("SELECT * FROM GAME")
+    resReplay = cursor.fetchall()
+    # Proceso de desconexión, en el fallo de la query de debajo
+    # se comprueba que la connexión no esta hecha
+    if conexion.is_connected():
+        conexion.disconnect()
+    # cursor.execute("SELECT * FROM ADVENTURE")
+    # res2 =cursor.fetchall()
 
-    cursor.execute("")
+    return res2
+
+print(getReplayAdventures())
