a6856c1ca5c86edb57e665f11a8f766e9a803b22
Albertfrgo <albertfr2021@outlook.es>
2022-01-17 12:50:08 +0100

    ReplayAdventures funcional

--- M3/getReplayAdventures.py
+++ M3/getReplayAdventures.py
@@ -3,9 +3,10 @@ from createCon import createConn
 def getReplayAdventures():
     conexion =createConn()
     cursor =conexion.cursor()
-    # Esta query es provisional para testear que la conexión se ha hecho correctamente,
-    # no es la del ejercicio
-    cursor.execute("SELECT * FROM GAME")
+    cursor.execute("SELECT GAME.ID_USER, Us.username, GAME.ID_ADVENTURE, Ad.adventure_name, GAME.ID_CHARACTER, Ch.character_name FROM GAME "
+                   "JOIN ADVENTURE Ad ON GAME.ID_ADVENTURE = Ad.ID_ADVENTURE "
+                   "JOIN USER Us ON Us.ID_USER = GAME.ID_USER "
+                   "JOIN CHARACTER Ch ON Ch.ID_CHARACTER = GAME.ID_CHARACTER;")
     resReplay = cursor.fetchall()
     # Proceso de desconexión, en el fallo de la query de debajo
     # se comprueba que la connexión no esta hecha
